62f5ebd5bf94fb136606d0f88c22847b9ec5c224
Victor Ferraz <vferraz48@gmail.com>
2023-07-02 17:35:48 -0300

    MindMate V1.01

---
+++ codigo/MindMate V1/codigo/MindMatev1.0/Indicações de Médicos e Clinicas/indicaçõesmed.js
@@ -0,0 +1,36 @@
+var dados = {
+    "medicos": [
+      {
+        "nome": "Dr. João Silva",
+        "especialidade": "Alzheimer"
+      },
+      {
+        "nome": "Dra. Maria Santos",
+        "especialidade": "Alzheimer"
+      },
+      {
+        "nome": "Dr. Pedro Alves",
+        "especialidade": "Alzheimer"
+      }
+    ],
+    "clinicas": [
+      {
+        "nome": "Mater Dei",
+        "endereco": "Rua A, 123",
+        "telefone": "(11) 1234-5678"
+      },
+      {
+        "nome": "Clínica Santa Maria",
+        "endereco": "Avenida B, 456",
+        "telefone": "(11) 9876-5432"
+      },
+      {
+        "nome": "Clínica Felicio Rocho",
+        "endereco": "Rua C, 789",
+        "telefone": "(11) 2468-1357"
+      }
+    ]
+  };
+  
+  console.log(dados);
+  
